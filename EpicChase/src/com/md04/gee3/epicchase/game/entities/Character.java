61fc835683889f621bc92de7b3cdd99061de92db
MD04-TanTan <md04_tantt@vtca.edu.vn>
2014-04-18 20:05:05 +0700

    Tân : 18/4 Update
    Update class Character

--- EpicChase/src/com/md04/gee3/epicchase/game/entities/Character.java
+++ EpicChase/src/com/md04/gee3/epicchase/game/entities/Character.java
@@ -1,9 +1,35 @@
 package com.md04.gee3.epicchase.game.entities;
 
-public class Character {
+import javax.microedition.lcdui.game.Sprite;
 
+public class Character {
+	
+	private int width;
+	private int height;
+	private Sprite spriteChar;
+	private int x;
+	private int y;
 	public Character() {
 		// TODO Auto-generated constructor stub
+		
 	}
+	
+	public void initImage()
+	{
+		
+	}
+	
+	public void jump() {
+		
+	}
+	
+	public void died() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
 
 }
