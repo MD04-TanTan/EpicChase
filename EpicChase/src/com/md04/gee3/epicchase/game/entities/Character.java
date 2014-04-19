a45c259612d17f7ec2d6ade884e4fd28af39bb6c
MD04-TanTan <md04_tantt@vtca.edu.vn>
2014-04-20 01:37:49 +0700

    20/4 Tân update
    Update class Character

--- EpicChase/src/com/md04/gee3/epicchase/game/entities/Character.java
+++ EpicChase/src/com/md04/gee3/epicchase/game/entities/Character.java
@@ -1,5 +1,8 @@
 package com.md04.gee3.epicchase.game.entities;
 
+import java.io.IOException;
+
+import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.Sprite;
 
 public class Character {
@@ -7,34 +10,66 @@ public class Character {
 	private int width;
 	private int height;
 	private Sprite spriteChar;
-	private int x;
-	private int y;
+	private Image imgChar;
+	private int frameWidth;
+	private int frameHeight;
+	
+	int curX;
+	int curY;
+	private  int  iJump;
 	public Character() {
 		// TODO Auto-generated constructor stub
-		
-		
+		initImage();
+		curX = 20;
+		curY = 230;
+		//iJump = curY - 50;
 	}
 	
-	public void initSprite()
+	//Tao sprite cho character
+	public Sprite createSprite()
 	{
-		
+		spriteChar = new Sprite(imgChar, 48, 48);
+		spriteChar.setPosition(curX, curY);
+		return spriteChar;
 	}
+	
+	//Khoi tao img
 	public void initImage()
 	{
-		
+		try {
+			imgChar = Image.createImage("/man.png");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	
+	//Ham nhay
 	public void jump() {
-		
+		iJump = curY-=5;
+		if(iJump >= 180)
+		{
+			spriteChar.setPosition(curX,iJump);
+		}
+			
 	}
 	
+	//Ham chet
 	public void died() {
 		
 	}
 	
 	public void update() {
 		
+		if(iJump <= 230)
+		{
+			iJump = curY+=5;
+			spriteChar.setPosition(curX, iJump);
+		}
+//		iJump = curY+=5;
+//		spriteChar.setPosition(curX, iJump);
+		//spriteChar.setPosition(curX, curY);
 	}
 	
-
 }
